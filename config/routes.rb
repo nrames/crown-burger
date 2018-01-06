4d3071a9a7750f7e127305610b4ef8df866372ef
SharpShinnedHawk <jasonglover5@gmail.com>
2018-01-06 14:59:53 -0700

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
-<<<<<<< HEAD
     resources :products, :meals, :locations
-=======
     resources :products, :meals, :locations, :carts
->>>>>>> architect
   end
 
   #Do not place any routes below this one
