45ebe3d32efe2d9736f06b5000111de0f9ca78ab
Nick Ames <nickrames@gmail.com>
2018-01-06 11:49:41 -0700

    cart component nav routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import Login from './Login';
 import Register from './Register';
 import Flash from './Flash';
 import Home from './Home';
+import Cart from './Cart';
 import ProtectedRoute from './ProtectedRoute';
 import AuthRoute from './AuthRoute';
 import FetchUser from './FetchUser';
@@ -21,6 +22,7 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <AuthRoute exact path='/login' component={Login} />
             <AuthRoute exact path='/register' component={Register} />
+            <ProtectedRoute exact path='/cart' component={Cart} />
             <Route component={NoMatch} />
           </Switch>
         </FetchUser>
